da5df61ff5efccddbba3d76b41aeb01dbf5229ae
Mohammadreza Ghorbani <reza@firstsource.email>
2020-09-01 09:47:31 +0430

    Set Currency padding fix

--- src/javascript/app/pages/user/set_currency.js
+++ src/javascript/app/pages/user/set_currency.js
@@ -100,7 +100,7 @@ const SetCurrency = (() => {
         const $fiat_currencies  = $('<div/>');
         const $cryptocurrencies = $('<div/>');
         currencies.forEach((c) => {
-            const $wrapper = $('<div/>', { class: 'gr-2 gr-3-m currency_wrapper', id: c });
+            const $wrapper = $('<div/>', { class: 'gr-2 gr-4-m currency_wrapper', id: c });
             const $image   = $('<div/>').append($('<img/>', { src: Url.urlForStatic(`images/pages/set_currency/${c.toLowerCase()}.svg`) }));
             const $name    = $('<div/>', { class: 'currency-name' });
 
