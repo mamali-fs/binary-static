0598077a2d6bacd9082f0b365f858af9ac1669d5
matin-binary <matin@binary.com>
2021-04-22 18:03:40 +0800

    Trading password reset copy update

--- src/templates/app/user/trading_reset_password.jsx
+++ src/templates/app/user/trading_reset_password.jsx
@@ -4,7 +4,7 @@ import { FormRow, Fieldset, SubmitButton } from '../../_common/components/forms.
 const TradingResetPassword = () => (
     <React.Fragment>
         <div className='static_full'>
-            <h1>{it.L('Trading Password Reset')}</h1>
+            <h1>{it.L('Trading password reset')}</h1>
             <p className='notice-msg invisible' id='msg_trading_reset_password' />
 
             <div className='invisible' id='form_error'>
@@ -15,13 +15,13 @@ const TradingResetPassword = () => (
             <div className='gr-parent gr-padding-10' id='container_trading_reset_password'>
                 <form id='frm_trading_reset_password'>
                     <Fieldset legend={it.L('Details')}>
-                        <p className='hint'>{it.L('Enter a new Trading password.')}</p>
+                        <p className='hint'>{it.L('Enter a new trading password.')}</p>
                         <FormRow
                             autoComplete='new-password'
                             has_password_meter
                             type='password'
                             id='new_password'
-                            label={it.L('New Trading password')}
+                            label={it.L('New trading password')}
                         />
                         <SubmitButton type='submit' no_error text={it.L('Confirm password')} />
                     </Fieldset>
